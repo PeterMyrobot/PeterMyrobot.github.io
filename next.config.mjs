73cf9352248e9fe35e17f71d766c81ba9df96a94
Peter Yang <ipod4928@gmail.com>
2024-05-14 22:16:42 +0800

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  output: 'export',
+  output: 'export', // <=== enables static exports
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
